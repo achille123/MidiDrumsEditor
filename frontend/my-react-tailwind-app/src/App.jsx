43d7bd53092f490da2c05004be4392c9c64c4aaf
Achille Chevrier <achille.chevrier@hotmail.com>
2025-06-01 10:12:06 +0200

    ok

---
+++ frontend/my-react-tailwind-app/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-blue-900 text-white flex justify-center items-center">
+      <h1 className="text-4xl font-bold">✅ Tailwind fonctionne !</h1>
+    </div>
+  );
+}
